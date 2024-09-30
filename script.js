a9a9f5606217d4f3bcaddad3133728328ac924cc
zmaplex <zmaplex@gmail.com>
2024-09-30 11:57:55 +0800

    Refactor file paths for manifest and service worker

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('service-worker.js')
         .then(registration => {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, error => {
